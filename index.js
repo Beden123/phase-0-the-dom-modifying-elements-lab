111edd360ea644720804eff39df45d563c4bb9f0
Beden <+>
2022-11-30 01:53:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = `<h1>Beden is the champion</h1>`;
